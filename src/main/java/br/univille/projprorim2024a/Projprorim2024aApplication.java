1e44aeacbcd18c4614adc5cbaac3eb43d28e70c4
Rafael Luiz Souza de Lima <isysrafael@gmail.com>
2024-04-10 19:57:28 -0300

    feat: Criação Projeto ProRim2024A - Branch main

---
+++ src/main/java/br/univille/projprorim2024a/Projprorim2024aApplication.java
@@ -0,0 +1,13 @@
+package br.univille.projprorim2024a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projprorim2024aApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projprorim2024aApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/univille/projprorim2024a/Projprorim2024aApplicationTests.java
@@ -0,0 +1,13 @@
+package br.univille.projprorim2024a;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projprorim2024aApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
